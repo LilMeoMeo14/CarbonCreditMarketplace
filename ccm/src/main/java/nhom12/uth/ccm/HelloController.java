6fd4c0c959bd77a5eb92373a560fdeb44c8b1ca6
Tommy <quangtrung54677@gmail.com>
2025-10-24 21:42:59 +0700

    Create project

---
+++ ccm/src/main/java/nhom12/uth/ccm/HelloController.java
@@ -0,0 +1,13 @@
+package nhom12.uth.ccm;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello world";
+    }
+
+}
